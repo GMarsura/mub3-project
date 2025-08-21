614c636bc803ab4d2858da9b9ad2926feffd7b8e
Gabriel <gabriel@MacBook-Air-de-Gabriel.local>
2025-08-21 20:42:11 -0300

    upd

---
+++ src/questions/questions.ts
@@ -0,0 +1,70 @@
+    const questions = 
+[   
+    {           
+        level: 1,
+        question: "tem pinguin no deserto do saará",
+        choices : [
+            {
+                text: "tem, mas são mutantes",
+                isTrue: false
+            },
+            {
+                text: "não, só vivem no nordeste mesmo",
+                isTrue: false
+            },
+            {
+                text: "não",
+                isTrue: true
+            },
+            {
+                text: "também não",
+                isTrue: true
+            }
+        ]
+    },
+    {           
+        level: 1,
+        question: "tem pinguin no deserto do saará2",
+        choices : [
+            {
+                text: "tem, mas são mutantes",
+                isTrue: false
+            },
+            {
+                text: "não, só vivem no nordeste mesmo",
+                isTrue: false
+            },
+            {
+                text: "não",
+                isTrue: true
+            },
+            {
+                text: "também não",
+                isTrue: true
+            }
+        ]
+    },
+    {           
+        level: 2,
+        question: "tem pinguin no deserto do saará3",
+        choices : [
+            {
+                text: "tem, mas são mutantes",
+                isTrue: false
+            },
+            {
+                text: "não, só vivem no nordeste mesmo",
+                isTrue: false
+            },
+            {
+                text: "não",
+                isTrue: true
+            },
+            {
+                text: "também não",
+                isTrue: true
+            }
+        ]
+    }
+
+];
